d0432610e5bd23fce1ee5351781aa52254e2d00e
gnarlytinmen <gnarlytinmen@users.noreply.github.com>
2017-09-12 21:03:40 -0700

    some figure clarity fixes

--- threeD_param_animate.m
+++ threeD_param_animate.m
@@ -1,10 +1,11 @@
-function [M] = threeD_param_animate(data,exportpath)
+function [M] = threeD_param_animate(data,degs)
 % 3D parameter map
 %%% CURRENTLY HARD CODED TO PLOT TUNING CENTER
 
 x = data;
 exportpath = '/home/tyler/Desktop/params_3D.avi';
 framerate = 25;
+rotation_mag = degs*2;  % Degrees, multiplied by 2 since camorbit is in half-degree steps
 
 figure1 = figure;
 set(gcf,'Position',[50 50 1300 900],'Color',[1 1 1]);
@@ -14,25 +15,28 @@ set(ax,'Units','pixels');
 hold on;
 xmax = round(max(max(x)));
 h = scatter3(x(:,2),x(:,3),x(:,4),65,'filled');
-plot3([0 xmax],[0 xmax],[0 xmax],'--k');
-xlabel('Normal Pursuit (Deg)');
-ylabel('Simulated Pursuit (Deg)');
-zlabel('Stabilized Pursuit (Deg)');
+plot3([0 xmax],[0 xmax],[0 xmax],'r','LineWidth',5);
+% xlabel('Normal Pursuit (Deg)');
+% ylabel('Simulated Pursuit (Deg)');
+% zlabel('Stabilized Pursuit (Deg)');
+xlabel('Normal Pursuit (Spks/s)');
+ylabel('Simulated Pursuit (Spks/s)');
+zlabel('Stabilized Pursuit (Spks/s)');
 set(gca,'XLim',[0,xmax],'YLim',[0,xmax],'ZLim',[0,xmax],'FontSize',18);
 set(gca,'XGrid','on','YGrid','on','ZGrid','on','GridAlpha',0.4,...
     'CameraPosition',[2700 -2000 1200],...
-    'CameraViewAngle',10);
+    'CameraViewAngle',3);
 
 axis vis3d
 
 M(120)=struct('cdata',[],'colormap',[]);
 
-for i = 1:60
+for i = 1:rotation_mag
     camorbit(0.5,0,'camera')
     drawnow
     M(i) = getframe(gcf, [0, 0, 1300, 900]);
 end
-for i = 1:60
+for i = 1:rotation_mag
     camorbit(-0.5,0,'camera')
     drawnow
     M(i+60) = getframe(gcf, [0, 0, 1300, 900]);
