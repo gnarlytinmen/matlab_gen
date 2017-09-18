a9c147404ca0dc7e5cf2cbc7d8bf2f2b1d3a091a
gnarlytinmen <gnarlytinmen@github.com>
2017-09-18 12:29:22 -0700

    added some var labels to more easily use with all bh fit parameters

--- threeD_param_animate.m
+++ threeD_param_animate.m
@@ -1,10 +1,11 @@
-function [M] = threeD_param_animate(data,exportpath)
+function [M] = threeD_param_animate(data,degs,units,filename)
 % 3D parameter map
 %%% CURRENTLY HARD CODED TO PLOT TUNING CENTER
 
 x = data;
-exportpath = '/home/tyler/Desktop/params_3D.avi';
+exportpath = ['/home/tyler/Desktop/',filename,'_paramfit_3D.avi'];
 framerate = 25;
+rotation_mag = degs*2;  % Degrees, multiplied by 2 since camorbit is in half-degree steps
 
 figure1 = figure;
 set(gcf,'Position',[50 50 1300 900],'Color',[1 1 1]);
@@ -13,26 +14,28 @@ set(ax,'Units','pixels');
 
 hold on;
 xmax = round(max(max(x)));
+camviewang = 3*xmax/120;
 h = scatter3(x(:,2),x(:,3),x(:,4),65,'filled');
-plot3([0 xmax],[0 xmax],[0 xmax],'--k');
-xlabel('Normal Pursuit (Deg)');
-ylabel('Simulated Pursuit (Deg)');
-zlabel('Stabilized Pursuit (Deg)');
+plot3([0 xmax],[0 xmax],[0 xmax],'r','LineWidth',5);
+unit_lab = {' (degs)',' (sp/s)'};
+xlabel(['Normal pursuit',unit_lab(units)]);
+ylabel(['Simulated pursuit',unit_lab(units)]);
+zlabel(['Stabilized pursuit',unit_lab(units)]);
 set(gca,'XLim',[0,xmax],'YLim',[0,xmax],'ZLim',[0,xmax],'FontSize',18);
 set(gca,'XGrid','on','YGrid','on','ZGrid','on','GridAlpha',0.4,...
     'CameraPosition',[2700 -2000 1200],...
-    'CameraViewAngle',10);
+    'CameraViewAngle',camviewang);
 
 axis vis3d
 
 M(120)=struct('cdata',[],'colormap',[]);
 
-for i = 1:60
+for i = 1:rotation_mag
     camorbit(0.5,0,'camera')
     drawnow
     M(i) = getframe(gcf, [0, 0, 1300, 900]);
 end
-for i = 1:60
+for i = 1:rotation_mag
     camorbit(-0.5,0,'camera')
     drawnow
     M(i+60) = getframe(gcf, [0, 0, 1300, 900]);
@@ -48,4 +51,3 @@ end
 close (writerObj);
 
 end
-
